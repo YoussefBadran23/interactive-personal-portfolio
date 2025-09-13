8ea006b0e86abbb388509c7a1e693c127c5b6276
Youssef Badran <youssef_m10239@cic-cairo.com>
2025-09-13 04:39:58 +0300

    Update CppIcon.tsx

--- components/icons/CppIcon.tsx
+++ components/icons/CppIcon.tsx
@@ -1,1 +1,7 @@
+import React from 'react';
 
+export const CppIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12.062 1.63H3.25c-1.38 0-2.5 1.12-2.5 2.5v16.24c0 1.38 1.12 2.5 2.5 2.5h8.813c1.38 0 2.5-1.12 2.5-2.5V4.13c0-1.38-1.12-2.5-2.5-2.5zM12.062 4.13v16.24H3.25V4.13h8.812zm-3.5 11.25h-2.5v-2.5h2.5v2.5zm0-3.5h-2.5v-2.5h2.5v2.5zm11.25-1.25h-2.5V6.63h2.5v3.25zm0 3.5h-2.5v-2.5h2.5v2.5z" />
+  </svg>
+);
